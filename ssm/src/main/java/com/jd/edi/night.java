901aae85c3768b7cc709ac44a49b254a3018e9ba
zhangjing <picklejing@gmail.com>
2022-05-02 09:51:00 +0800

    第六次提交

---
+++ ssm/src/main/java/com/jd/edi/night.java
@@ -0,0 +1,12 @@
+package com.jd.edi;
+
+import lombok.Data;
+
+@Data
+public class night {
+    private String name;
+    private String address;
+    private String age;
+    private String sex;
+
+}
